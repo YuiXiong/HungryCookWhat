c34686750a65315c74c0e4d3cd3233c00eced085
Yui Xiong Ng <jadebear15@gmail.com>
2022-06-26 11:52:25 +0800

    search function done working on recipe's own page n css

--- main.js
+++ main.js
@@ -1,67 +1,75 @@
-const randomUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+const randomUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+
 const mealsElement = document.getElementById("meals-id");
+const searchTerm = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
 
 fetchDataAsync(randomUrl);
 
 async function fetchDataAsync(randomUrl) {
   try {
-    const response = await fetch(randomUrl)
-    const data = await response.json()
-    const randomMeal = data.meals[0]
-    addMeal(randomMeal)
-    addMeal(randomMeal)
+    const response = await fetch(randomUrl);
+    const data = await response.json();
+    const randomMeal = data.meals[0];
     //to run add random meal function here in future
-
-    //omit here in future
-    console.log("data: ", randomMeal)
-    console.log("instructions", data.meals[0].strInstructions)
-    const instructions = data.meals[0].strInstructions
-    console.log(instructions)
-    addTextToDom(instructions, "p", ".card-text-1");
-
-
+    addMeal(randomMeal);
   } catch (error) {
-    console.log(`${error}`)
+    console.log(`${error}`);
   }
-
 }
-
-//addTextToDom probably remove in future since it will be build into fn
-const addTextToDom = (text, childSelector, parentSelector) => {
-  const childElement = document.createElement(childSelector)
-  const parentElement = document.querySelector(parentSelector)
-  childElement.innerText = text
-  parentElement.appendChild(childElement)
-};
 
 //add random meal and append to card function
 function addMeal(mealData) {
-  console.log(mealData);
-
   const meal = document.createElement("div");
-  meal.classList.add("col-sm-3");
+  meal.classList.add("col-sm-4");
 
   meal.innerHTML = `
 
-  <div class="card" style="width: 18rem;">
+  <div class="card" style="width: 100%">
         <div class="meal-header">
-            ${`
-            <span class="random"> Random Recipe 1</span>`
-            }
+            <span class="random">Recipe for the lost</span>
+            <h5 class="card-title">${mealData.strMeal}</h5>
             <img class="card-img-1 img-fluid"
                 src="${mealData.strMealThumb}"
                 alt="${mealData.strMeal}"
             />
         </div>
         <div class="card-body">
-            <h5 class="card-title">${mealData.strMeal}</h5>
-            <a href="${mealData.strSource}" class="btn btn-primary">Go to source</a>
+            <a href = "javascript:void(0)" onclick= "fetchById(${mealData["idMeal"]})" data-id =${mealData["idMeal"]} class="btn btn-primary">Go to source</a>
 
         </div>
         </div>
         </div>
     `;
-
-
   mealsElement.appendChild(meal);
 }
+
+//search function here
+async function getMealsBySearch(term) {
+  try{
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
+  );
+
+  const respData = await resp.json();
+  const meals = respData.meals;
+
+  return meals;
+} catch (error) {
+  console.log(`${error}`);
+}
+}
+
+searchBtn.addEventListener("click", async () => {
+  //for cleaning container, might wanna remove it during styling of site
+  mealsElement.innerHTML = "";
+
+  const search = searchTerm.value;
+  const meals = await getMealsBySearch(search);
+
+  if (meals) {
+    meals.forEach((meal) => {
+      addMeal(meal);
+    });
+  }
+});
